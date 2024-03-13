b6a26cefd39b2e74d6b55f6380fed4c0306ef43f
Jakob Buchsteiner <jakob.buchsteiner@gmail.com>
2024-03-13 14:34:52 +0100

    Add tuw_spike_demo package

---
+++ tuw_spike_demo/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'tuw_spike_demo'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jakob',
+    maintainer_email='jakob.buchsteiner@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'twist_buildhat = tuw_spike_demo.twist_buildhat:main'
+        ],
+    },
+)
