fdafa33e16a0738210c586977ff84facf5714074
moritz <e12102632@student-tuwien.ac.at>
2024-04-24 08:19:58 +0200

    added model already exists to spawner

--- tuw_simulation/launch/spawn_robot.launch.py
+++ tuw_simulation/launch/spawn_robot.launch.py
@@ -1,20 +1,19 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, RegisterEventHandler, LogInfo
 from launch.substitutions import PathJoinSubstitution, TextSubstitution
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-import launch_ros.actions
 from launch.substitutions import Command, LaunchConfiguration, FindExecutable
-from ament_index_python.packages import get_package_share_directory
 
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 import os
 
+from launch.event_handlers import (OnExecutionComplete, OnProcessExit,
+                                OnProcessIO, OnProcessStart, OnShutdown)
+
 
 def generate_launch_description():
 
     use_sim_time     = LaunchConfiguration('use_sim_time',  default='true')
-    namespace_arg    = DeclareLaunchArgument('namespace',   default_value=TextSubstitution(text=''))
     model_name_arg   = DeclareLaunchArgument('model_name',  default_value=TextSubstitution(text='robot0'))
     X_launch_arg     = DeclareLaunchArgument('X',           default_value=TextSubstitution(text='0.0'))
     Y_launch_arg     = DeclareLaunchArgument('Y',           default_value=TextSubstitution(text='0.0'))
@@ -38,14 +37,6 @@ def generate_launch_description():
         ]
     )
     
-    params = {'robot_description': robot_description_content}
-    robot_state_publisher = launch_ros.actions.Node(package='robot_state_publisher',
-                                  executable='robot_state_publisher',
-                                  output='both',
-                                  parameters=[params],
-                                  arguments="urdf",
-                                  namespace=[LaunchConfiguration('model_name')],)    
-   
     spawner = Node(
         package="tuw_simulation",
         executable="spawn_robot.py",
@@ -57,6 +48,14 @@ def generate_launch_description():
         arguments=[robot_description_content]
     )
     
+    params = {'robot_description': robot_description_content}
+    robot_state_publisher = Node(package='robot_state_publisher',
+                                  executable='robot_state_publisher',
+                                  output='both',
+                                  parameters=[params],
+                                  arguments="urdf",
+                                  namespace=[LaunchConfiguration('model_name')],)  
+    
     tuw_simulation = FindPackageShare("tuw_simulation")
     bridge_config = PathJoinSubstitution([tuw_simulation, "world", "tuw_simulation_bridge.yaml"])
     
@@ -66,14 +65,24 @@ def generate_launch_description():
         parameters=[{"config_file": bridge_config}, {'use_sim_time': True}],
         namespace=[LaunchConfiguration("model_name")]
     )
+            
+    def error_handling(event):
+        code = event.text.decode().strip()
+        if code == '0':
+            return [bridge, robot_state_publisher]
+        else:
+            return LogInfo(msg=f"aborting launch")
 
     return LaunchDescription(
         [
         X_launch_arg,
         Y_launch_arg,
         model_name_arg,
-        namespace_arg,
-        bridge,
-        robot_state_publisher,
         spawner,
+        RegisterEventHandler(
+            OnProcessIO(
+                target_action=spawner,
+                on_stdout=error_handling
+            )
+        )
     ])
--- tuw_simulation/scripts/spawn_robot.py
+++ tuw_simulation/scripts/spawn_robot.py
@@ -11,7 +11,12 @@ from rclpy.task import Future
 import subprocess
 import re
 
-
+def get_model_list(string):
+    befor, sep, after = string.partition(":")
+    temp = after.split("\n")
+    temp = ' '.join(temp).split()
+    temp = [x for x in temp if x != '-']
+    return temp
 
 
 def main(args=None):
@@ -23,26 +28,42 @@ def main(args=None):
     x = node.get_parameter('X').get_parameter_value().double_value
     y = node.get_parameter('Y').get_parameter_value().double_value
     z = 0.4
+    error_code = 0
     
     with open("/tmp/robot.urdf", "w") as f:
         f.write(sys.argv[1])
     
     model_name = node.get_parameter('model_name').get_parameter_value().string_value
-    node.get_logger().info("model_name: " + str(model_name))
     
-    subprocess.run([
-        "ign", "service",
-        "-s", "/world/plain_world/create",
-        "--reqtype", "ignition.msgs.EntityFactory",
-        "--reptype", "ignition.msgs.Boolean",
-        "--timeout", "1000",
-        "--req", f'sdf_filename: "/tmp/robot.urdf", name: "{model_name}",  pose: {{ position: {{x: {x}, y: {y}, z: {z}}} }}'
-        ], stdout=subprocess.DEVNULL, check=True)
+    result = subprocess.run([
+        "ign", "model", "--list"
+    ], stdout = subprocess.PIPE, text=True)
+    
+    temp = get_model_list(result.stdout)
+    if temp.__contains__(model_name):
+        node.get_logger().info("model already exists")
+        error_code = 1
+    elif (len(temp) == 0):
+        node.get_logger().info("world not existing")
+        error_code = 2
+    else:
+        node.get_logger().info("create model with name: " + str(model_name))
+        
+    
+    if error_code == 0:
+        subprocess.run([
+            "ign", "service",
+            "-s", "/world/plain_world/create",
+            "--reqtype", "ignition.msgs.EntityFactory",
+            "--reptype", "ignition.msgs.Boolean",
+            "--timeout", "1000",
+            "--req", f'sdf_filename: "/tmp/robot.urdf", name: "{model_name}",  pose: {{ position: {{x: {x}, y: {y}, z: {z}}} }}'
+            ], stdout=subprocess.DEVNULL, check=True)
     
     
     node.destroy_node()
     rclpy.shutdown()
-
+    print(error_code)
 
 if __name__ == '__main__':
     main()
