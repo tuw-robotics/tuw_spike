58845865aebb73194cd26429471cfe006b8a8399
Jakob Buchsteiner <jakob.buchsteiner@gmail.com>
2024-06-27 18:03:52 +0200

    Run camera_to_laserscan in check_camera_alignment

--- tuw_spike_analysis/launch/check_camera_alignment.launch.py
+++ tuw_spike_analysis/launch/check_camera_alignment.launch.py
@@ -1,15 +1,11 @@
-from launch_ros.actions import LoadComposableNodes, PushRosNamespace, SetParameter, Node, LifecycleNode
-from launch_ros.descriptions import ComposableNode, ParameterFile
+from launch_ros.actions import PushRosNamespace, SetParameter, Node, LoadComposableNodes
 from launch_ros.substitutions import FindPackageShare
+from launch_ros.descriptions import ComposableNode, ParameterFile
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, GroupAction
-from launch.substitutions import PathJoinSubstitution, LaunchConfiguration, AndSubstitution
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.conditions import IfCondition, LaunchConfigurationEquals, LaunchConfigurationNotEquals
-from launch_ros.events.lifecycle import ChangeState
-from launch.events.matchers import matches_action
-
-from lifecycle_msgs.msg import Transition
+import math
 
 
 def robot_ns_from_hostname():
@@ -53,7 +49,39 @@ def generate_launch_description():
 
     optitrack_to_tf = Node(
         package="tuw_spike_analysis",
-        executable="optitrack_to_tf"
+        executable="optitrack_to_tf",
+        parameters=[{
+            "frame": "optitrack",
+            "child_frame": "base_odom"
+        }]
+    )
+    
+    static_tf = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        arguments=[
+            "--frame-id", "map", "--child-frame-id", "optitrack",
+            "--yaw", str(math.radians(0.0))
+        ],
+        remappings=[
+            ("/tf", "tf"),
+            ("/tf_static", "tf_static")
+        ]
+    )
+
+    camera_proc_container = [
+        LaunchConfiguration("ros_namespace"), "/camera_processing_container"
+    ]
+    localizer_comp = ComposableNode(
+        package='tuw_camera_laserscan',
+        plugin='tuw_camera_laserscan::RayLocalizerNode',
+        name='ray_localizer',
+        extra_arguments=[{'use_intra_process_comms': True}],
+        parameters=[ParameterFile(PathJoinSubstitution([tuw_camera_laserscan, "config", "localizer.yaml"]))],
+        remappings=[
+            ('/tf', 'tf'),
+            ('/tf_static', 'tf_static')
+        ]
     )
 
     return LaunchDescription([
@@ -63,8 +91,13 @@ def generate_launch_description():
         GroupAction([
             PushRosNamespace(LaunchConfiguration("robot_ns")),
             capture_replay_launch,
+            static_tf,
             optitrack_to_tf,
             map_server,
-            lifecycle_manager
+            lifecycle_manager,
+            LoadComposableNodes(
+                target_container=camera_proc_container,
+                composable_node_descriptions=[localizer_comp]
+            )
         ])
     ])
--- tuw_spike_analysis/tuw_spike_analysis/optitrack_to_tf.py
+++ tuw_spike_analysis/tuw_spike_analysis/optitrack_to_tf.py
@@ -17,19 +17,23 @@ class OptitrackToTF(Node):
     def __init__(self):
         super().__init__(f"optitrack_to_tf")
 
-        self.time_offset = self.declare_parameter("time_offset", 0.05).value
+        self.time_offset: float = self.declare_parameter("time_offset", -0.03).value
+        self.frame: str = self.declare_parameter("frame", "").value
+        self.child_frame: str = self.declare_parameter("child_frame", "").value
         self.sub = self.create_subscription(Odometry, "odom_ground_truth", self.odom_callback, 10)
         self.pub = self.create_publisher(TFMessage, "tf", 10)
         
     def odom_callback(self, msg: Odometry):
-        tf = TransformStamped()
-        tf.header.frame_id = "map"
         tf_time = Time.from_msg(msg.header.stamp) + Duration(seconds=self.time_offset)
+
+        tf = TransformStamped()
         tf.header.stamp = tf_time.to_msg()
-        tf.child_frame_id = "base_odom"
+        tf.header.frame_id = self.frame if self.frame else msg.header.frame_id
+        tf.child_frame_id = self.child_frame if self.child_frame else msg.child_frame_id
         tf.transform.translation.x = msg.pose.pose.position.x
         tf.transform.translation.y = msg.pose.pose.position.y
         tf.transform.rotation = quat_mul(msg.pose.pose.orientation, Quaternion(x=1.0, y=0.0, z=0.0, w=0.0))
+
         self.pub.publish(TFMessage(transforms=[tf]))
 
 def main(args=None):
