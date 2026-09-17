18c6d9d6d40105fb8471ff67ee6d1ca6a6603b78
Markus Bader <markus.bader@tuwien.ac.at>
2026-09-17 14:53:09 +0000

    motor speed pwm in perscent

--- tuw_build_hat/src/tuw_build_hat/motor.cpp
+++ tuw_build_hat/src/tuw_build_hat/motor.cpp
@@ -16,8 +16,8 @@ const std::string &Motor::info() const {
 }
 
 void Motor::init() {
-    cmd_ += std::format("plimit {}; ", plimit());
     cmd_ += std::format("port {}; ", port_);
+    cmd_ += std::format("port_plimit {}; ", plimit()); // port_plimit only sets the limt on the selected port
     cmd_ += std::format("combi 0 {} 0 {} 0 {} 0; ", BuildHat::MODE_SPEED, BuildHat::MODE_POS, BuildHat::MODE_APOS);
     cmd_ += std::format("select 0; ");             // streams the whole payload
     cmd_ += std::format("selrate {}; ", selrate_); // updates rate
@@ -35,20 +35,24 @@ void Motor::set_target_radian_per_sec(double speed, double kp, double ki, double
 
     int percent = Device::convert_speed_to_percent(speed_target_, MAX_RAD_PER_SEC);
 
-    int pvport = port_;           // Port to read the process variable from
-    int pvmode = 0;               // Mode to read from. Here that's the combi mode 0 you selected.
-    int pvoffset = 0;             // Byte offset in that mode's data. Speed is at byte 0.
-    const char *pvformat = "s1";  // Signed byte. Speed is an integer from −100 to +100 %.
-    double pvscale = 1.0;         // Multiplier applied to the raw value. Use it to convert % into rad/s.
-    int pvunwrap = 0;             // 0 means no phase unwrapping, which is what you want for speed
-    kp_ = kp;                     // Proportional gain (error → drive level from −1 to 1)
-    ki_ = ki;                     // Integral gain. The time base is 1 s.
-    kd_ = kd;                     // Derivative gain. The time base is 1 s.
-    int windup = 100;             // Absolute clamp on the integrator
-    double deadzone = 0.01;       // Error band (rad/s) where the controller does not react
+    int pvport = port_;          // Port to read the process variable from
+    int pvmode = 0;              // Mode to read from. Here that's the combi mode 0 you selected.
+    int pvoffset = 0;            // Byte offset in that mode's data. Speed is at byte 0.
+    const char *pvformat = "s1"; // Signed byte. Speed is an integer from −100 to +100 %.
+    double pvscale = 1.0;        // Multiplier applied to the raw value. Use it to convert % into rad/s.
+    int pvunwrap = 0;            // 0 means no phase unwrapping, which is what you want for speed
+    kp_ = kp;                    // Proportional gain (error → drive level from −1 to 1)
+    ki_ = ki;                    // Integral gain. The time base is 1 s.
+    kd_ = kd;                    // Derivative gain. The time base is 1 s.
+    int windup = 100;            // Absolute clamp on the integrator
+    double deadzone = 0.01;      // Error band (rad/s) where the controller does not react
 
+    // pwmparams configure parameters for PWM driver
+    double pwmthresh = 0.65; // threshold for slow/fast PWM switchover (default 0)
+    double minpwm = 0.01;    // minimum PWM driver input value (default 0)
     cmd_ += std::format("port {}; ", port_);
-    cmd_ += std::format("pid {} {} {} {} {} {} {} {} {} {} {}; ", port_, pvmode, pvoffset, pvformat, pvscale, pvunwrap, kp_, ki_, kd_, windup, deadzone); // updates rate
+    cmd_ += std::format("pwmparams {} {}", pwmthresh, minpwm);                                                                                            // configure parameters for PWM driver
+    cmd_ += std::format("pid {} {} {} {} {} {} {} {} {} {} {}; ", pvport, pvmode, pvoffset, pvformat, pvscale, pvunwrap, kp_, ki_, kd_, windup, deadzone); // updates rate
     cmd_ += std::format("set {}; ", percent);
     cmd_ += std::format("\r");
 }
@@ -86,7 +90,6 @@ int Motor::decode(const std::string &line) {
     cumulative_rad_ = Device::convert_deg_to_rad(cumulative_deg);
     absolute_rad_ = Device::normalize_rad(Device::convert_deg_to_rad(absolute_deg));
 
-    std::cout << port_ << ": " << rps_ << ", " << cumulative_rad_ << ", " << absolute_rad_ << std::endl;
     return BuildHat::DECODE_OK;
 }
 } // namespace tuw_build_hat
